7673c9a0f258885310a2c19bfc7731103a629519
Hemanthkumar2k04 <hemanthkumar2k04@gmail.com>
2025-09-14 20:15:27 +0530

    Removed Front-end Part

--- train-fittings/src/components/ui/aspect-ratio.tsx
+++
@@ -1,6 +0,0 @@
-// train-fittings/src/components/ui/aspect-ratio.tsx
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio"
-
-const AspectRatio = AspectRatioPrimitive.Root
-
-export { AspectRatio }
--- train-fittings/src/pages/Index.tsx
+++
@@ -1,9 +0,0 @@
-// Update this page (the content is just a fallback if you fail to update the page)
-// src/pages/Index.tsx
-import { AssetDashboard } from "@/components/AssetDashboard";
-
-const Index = () => {
-  return <AssetDashboard />;
-};
-
-export default Index;
